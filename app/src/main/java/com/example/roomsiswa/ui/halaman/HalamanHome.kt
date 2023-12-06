01cb6bca1b725f5b6470c98acc5c01f5d9eefdb8
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-12-06 16:08:56 +0700

    create obj DestinasiHome in HalamanHome

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package com.example.roomsiswa.ui.halaman
 
+import com.example.roomsiswa.R
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
