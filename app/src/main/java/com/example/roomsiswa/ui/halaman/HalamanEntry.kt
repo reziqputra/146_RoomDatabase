7e97bc36d819580ec438e81925fa0b2f8c7178ca
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-12-06 15:22:26 +0700

    create object DestinasiEntry in HalamanEntry

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,2 +1,9 @@
 package com.example.roomsiswa.ui.halaman
 
+import com.example.roomsiswa.R
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
