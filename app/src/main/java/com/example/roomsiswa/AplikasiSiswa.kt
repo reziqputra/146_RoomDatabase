00b0df405c6043ca4b3c19811b6a485acee21bd6
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-12-06 13:43:38 +0700

    create class AplikasiSiswa

--- app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
@@ -1,4 +1,14 @@
 package com.example.roomsiswa
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.roomsiswa.repositori.ContainerApp
+import com.example.roomsiswa.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application(){
+    lateinit var container: ContainerApp
+
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
